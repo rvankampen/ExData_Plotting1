dc9876be428226dd5575d2a4946121422b266954
Rich Van Kampen <rvankampen@users.noreply.github.com>
2016-03-13 23:26:35 -0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -11,21 +11,6 @@ subset$DateTime <- strptime(paste(subset$Date, subset$Time, sep=" "),
 
 # Plot "subset"
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(subset, {
-        plot(Global_active_power~DateTime, type="l", 
-             ylab="Global Active Power (kilowatts)", xlab="")
-        plot(Voltage~DateTime, type="l", 
-             ylab="Voltage (volt)", xlab="")
-        plot(Sub_metering_1~DateTime, type="l", 
-             ylab="Global Active Power (kilowatts)", xlab="")
-        lines(Sub_metering_2~DateTime,col='Red')
-        lines(Sub_metering_3~DateTime,col='Blue')
-        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-               legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-        plot(Global_reactive_power~DateTime, type="l", 
-             ylab="Global Rective Power (kilowatts)",xlab="")
-})
-par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 # 1
 plot(subset$DateTime, subset$Global_active_power, type="l",
      xlab="", ylab="Global Active Power")
